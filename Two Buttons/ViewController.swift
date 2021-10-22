20c0f380c57000f7b64906fbb2429f95bc7ec76a
AB Moreno <rappygod678@gmail.com>
2021-10-22 09:14:13 -0400

    Initial Commit

---
+++ Two Buttons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two Buttons
+//
+//  Created by AB Hernandez on 10/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
